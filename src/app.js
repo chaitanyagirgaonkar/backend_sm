207228e42839f6d972f041e8b526948e83b72523
Chaitanya girgaonkar <94917737+chaitanyagirgaonkar@users.noreply.github.com>
2024-05-29 22:26:14 +0530

    Update app.js from github

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+   origin: 'https://eduscribee.vercel.app',
+  credentials: true
 }))
 
 app.use(express.json({ limit: "16kb" }))
